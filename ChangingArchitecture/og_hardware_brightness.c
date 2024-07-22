290b76696c8775024f4432df9b28eaafeb692073
Osbaldo Gonzalez Jr <osbaldogonzalezjr@gmail.com>
2024-07-22 12:09:52 -0700

    Cut og_hardware_brightness to min specification
    Lean version that only changes brightness, have yet to test though!

--- ChangingArchitecture/og_hardware_brightness.c
+++ ChangingArchitecture/og_hardware_brightness.c
@@ -6,77 +6,49 @@
 #include <sys/stat.h>
 
 /**
- * og_hardware_dim meant to be used with displays whose backlight can be
- * adjusted through software, usually laptops
- */
+ * og_hardware_brightness is meant to be used with displays whose
+ * backlight can be adjusted through software, usually laptops
+**/
+
+
+/**
+ * brightness control file should not be changing often. This avoids
+ * having to take in input to decide which file to write to which is
+ * scary when this is meant to be run as root with set uid.
+**/
+#ifndef HW_BRIGHTNESS_CTL_FNAME
+    #error Please use -D during compilation to define HW_BRIGHTNESS_CTL_FNAME where it evaluates to path for backlight control file
+#endif
+
+#ifndef HW_BRIGHTNESS_MAX_INT
+    #error Please use -D during compilation to define HW_BRIGHTNESS_MAX_INT where it evaluates int value for max brightness allowed
+#endif
+
+#ifndef HW_BRIGHTNESS_MIN_INT
+    #error Please use -D during compilation to define HW_BRIGHTNESS_MIN_INT where it evaluates int value for min brightness allowed
+#endif
+
+#define STRINGIFY(name) #name
 
 int main(const int argc, const char *const *argv);
 bool isFileOnlyControlledByRoot(const char* fname);
 FILE* fileOpener(const char* const fname, const char* const mode, int error_code);
-void changeFileMode(FILE* fhandle, const char* fpath, const char* mode, int error_code);
 int clamp(int value, int min_inclusive, int max_inclusive);
 
 // such a short lil script, will let OS cleanup by itself (files and memory).
 // executable is expected to owned by root and have setuid active in file permissions, else can't edit brightness file
 int main(const int argc, const char *const *argv)
 {
-    if (!(argc > 4)) {
-        fprintf(stderr, "Usage: %s <DIM BRIGHTNESS VALUE> <STORE CURRENT HARDWARE BRIGHTNESS FILE> <HARDWARE BRIGHTNESS CONTROL FILE> <MAX HARDWARE BRIGHTNESS FILE>\n", argv[0]);
+    if (!(argc > 1)) {
+        fprintf(stderr, "Usage: %s <BRIGHTNESS INT VALUE>\n", argv[0]);
         return 0;
     }
 
-    int error_code_num = 1;
-    const int user_input_dim_brightness_value = atoi(argv[1]);
-    const char* const pre_dimstate_brightness_fname = argv[2];
-    const char* const curr_brightness_ctl_fname = argv[3];
-    const char* const max_brightness_fname = argv[4];
-    FILE* const pre_dimstate_brightness_fhandle = fileOpener(pre_dimstate_brightness_fname, "r", error_code_num++);
-    FILE* const curr_brightness_ctl_fhandle = fileOpener(curr_brightness_ctl_fname, "r", error_code_num++);
-    FILE* const max_brightness_fhandle = fileOpener(max_brightness_fname, "r", error_code_num++);
+    const int brightness = clamp(atoi(argv[1]), HW_BRIGHTNESS_MIN_INT, HW_BRIGHTNESS_MAX_INT);
+    const char* const brightness_ctl_fname = argv[3];
+    FILE* const brightness_ctl_fhandle = fileOpener(STRINGIFY(HW_BRIGHTNESS_CTL_FNAME), "w", 1);
+    fprintf(brightness_ctl_fhandle, "%d", brightness);
 
-    const int MIN_BRIGHTNESS = 1;
-    int pre_dimstate_brightness = 1;
-    int curr_brightness = 1;
-    int MAX_BRIGHTNESS = 1;
-
-    if (fscanf(max_brightness_fhandle, "%d", &MAX_BRIGHTNESS) < 1) {
-        fprintf(stderr, "ERROR: Max brightness integer value not found in %s\n", max_brightness_fname);
-        exit(error_code_num++);
-    }
-
-    if (fscanf(curr_brightness_ctl_fhandle, "%d", &curr_brightness) < 1) {
-        fprintf(stderr, "ERROR: Current brightness integer value not found in %s\n", curr_brightness_ctl_fname);
-        exit(error_code_num++);
-    }
-    {
-        const int old_curr_brightness = curr_brightness;
-        curr_brightness = clamp(curr_brightness, MIN_BRIGHTNESS, MAX_BRIGHTNESS);
-        if (curr_brightness != old_curr_brightness) fprintf(stderr, "WARNING: Current brightness value was invalid! Changed to %d. Value was: %d\n", curr_brightness, old_curr_brightness);
-    }
-
-    bool is_already_dim = (fscanf(pre_dimstate_brightness_fhandle, "%d", &pre_dimstate_brightness) == 1);
-    {
-        // just incase user accidentally/maliciously changed value in pre_dimstate_brightness file
-        const int old_pre_dimstate_brightness = pre_dimstate_brightness;
-        pre_dimstate_brightness = clamp(pre_dimstate_brightness, MIN_BRIGHTNESS, MAX_BRIGHTNESS);
-        if (pre_dimstate_brightness != old_pre_dimstate_brightness) fprintf(stderr, "WARNING: Pre dimstate brightness value was invalid! Changed to %d. Value was: %d\n", pre_dimstate_brightness, old_pre_dimstate_brightness);
-    }
-    // clear files contents to get ready for replacement values
-    changeFileMode(curr_brightness_ctl_fhandle, curr_brightness_ctl_fname, "w", error_code_num++);
-    changeFileMode(pre_dimstate_brightness_fhandle, pre_dimstate_brightness_fname, "w", error_code_num++); // pre dimstate file is to be empty when not in dimstate
-
-    if (is_already_dim) {
-        fprintf(curr_brightness_ctl_fhandle, "%d", pre_dimstate_brightness);
-        printf("Set brightness back to: %d\n", pre_dimstate_brightness);
-        return 0;
-    }
-
-    const int DIM_BRIGHTNESS_VALUE = clamp(user_input_dim_brightness_value, MIN_BRIGHTNESS, MAX_BRIGHTNESS);
-    if (user_input_dim_brightness_value != DIM_BRIGHTNESS_VALUE) fprintf(stderr, "WARNING: Input dim brightness value was invalid! Changed to %d. Value was: %d\n", DIM_BRIGHTNESS_VALUE, user_input_dim_brightness_value);
-
-    fprintf(pre_dimstate_brightness_fhandle, "%d", curr_brightness);
-    fprintf(curr_brightness_ctl_fhandle, "%d", DIM_BRIGHTNESS_VALUE);
-    printf("Dimmed brightness to: %d\n", DIM_BRIGHTNESS_VALUE);
     return 0;
 }
 
@@ -99,10 +71,11 @@ bool isFileOnlyControlledByRoot(const char* fname)
 
 FILE* fileOpener(const char* const fname, const char* const mode, int error_code)
 {
-    // since program is expected to have set uid on need to be stricter.
-    // done to reduce attack vectors and minimize accidental changes of
-    // files outside of og_hardware_dim program don't require write
-    // permissions here since files defining max_brightness are often read only
+    /** since program is expected to have set uid on, need to be stricter.
+     * done to reduce attack vectors and minimize accidental changes of
+     * files outside of og_hardware_brightness program don't require write
+     * permissions here since files defining max_brightness are often read only
+    **/
     if (!isFileOnlyControlledByRoot(fname)) {
         fprintf(stderr,
         "ERROR: %s must meet the following requirements:\n"
@@ -120,22 +93,13 @@ FILE* fileOpener(const char* const fname, const char* const mode, int error_code
     FILE* fp = fopen(fname, mode);
 
     if (fp == NULL) {
-        fprintf(stderr, "ERROR: Wasn't able to open: %s\n", fname);
+        fprintf(stderr, "ERROR: %s, Wasn't able to open: %s\n", strerror(errno), fname);
         exit(error_code);
     }
 
     return fp;
 }
 
-void changeFileMode(FILE* fhandle, const char* fpath, const char* mode, int error_code)
-{
-    FILE* fp = freopen(NULL, mode, fhandle);
-
-    if (fp == NULL) {
-        fprintf(stderr, "ERROR: %s, Wasn't able to change file mode to '%s' on %s\n", strerror(errno), mode, fpath);
-        exit(error_code);
-    }
-}
 
 /**
  * @brief clamp value to be within min_inclusive and max_inclusive
